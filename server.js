72e8a851005ed3b0fdc6cd5fad6f07c82d99f99b
Adrian Baltag <fbaltag83@gmail.com>
2023-04-23 16:03:48 -0400

    setup port

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const express = require("express");
+// allows us to have a .env file with files in it
+const dotenv = require("dotenv").config();
+// access to the port number(from .env file; if not available, use 5000 )
+const port = process.env.PORT || 5000;
+
+// initialize express
+const app = express();
+// initialize
+app.listen(port, () => console.log(`Server running on port ${port}`));
